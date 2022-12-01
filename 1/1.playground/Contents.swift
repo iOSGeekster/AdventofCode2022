4a1dec8b8781f84fa5ed02760e106e0f53a8b301
Jesper Westervig Nielsen <jesper.westervig.nielsen@LEGO.com>
2022-12-01 06:47:00 +0100

    December 1st

---
+++ 1/1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
+
+let numbers: [String] = try String(contentsOf: url, encoding: String.Encoding.utf8).components(separatedBy: "\n").map {
+    $0
+}
+
+var highest = 0
+var highest2 = 0
+var highest3 = 0
+
+let bundled = numbers.split(separator: "")
+
+for test in bundled {
+    let calc = test.map { Int($0)! }.reduce(0, +)
+    if calc > highest {
+        highest3 = highest2
+        highest2 = highest
+        highest = calc
+    } else if calc > highest2 {
+        highest3 = highest2
+        highest2 = calc
+    } else if calc > highest3 {
+        highest3 = calc
+    }
+}
+
+print(highest)
+print(highest+highest2+highest3)
